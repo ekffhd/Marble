b2e49a8c8ccae7eb124dd3ebd35ce358e9d99cc4
James <jamesj0107@naver.com>
2018-12-03 02:44:16 +0900

    Add background music play continuously

--- src/Marble.java
+++ src/Marble.java
@@ -1,7 +1,22 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
+import java.io.File;
 
 public class Marble {
     public static void main(String[] args){
+        try{
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("Music.wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
+        catch (Exception except){
+            System.out.println("Error with playing sound.");
+            except.printStackTrace();
+        }
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
