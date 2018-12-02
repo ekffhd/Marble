a626635f6c5c279cfb1396371ee8f5fee79c6a85
James <jamesj0107@naver.com>
2018-12-02 18:03:09 +0900

    Init class City

---
+++ src/City.java
@@ -0,0 +1,35 @@
+public class City implements IPlace{
+    private String name;
+    private Building[] buildings;
+    private int price;
+    private Player owner;
+
+    public City(){
+        buildings = new Building[4];
+        price = 0;
+        for(int i=0; i<4; i++){
+            buildings[i] = new Building();
+        }
+    }
+
+    private class Building implements IBuilding{
+        public void purchase(){
+
+        }
+        public void sell(){
+
+        }
+        public void acquire(){
+
+        }
+    }
+
+    public void onEnter(){
+        if (owner == null){ // 주인이 없을 떄
+
+        }
+        else { // 주인이 있을 때
+
+        }
+    }
+}
