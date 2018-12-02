cc65e6f6ee667f21dd096facff67563624aa03e3
James <jamesj0107@naver.com>
2018-12-03 01:02:24 +0900

    Add boolean is_active to PlayerData and create getters

--- src/PlayerData.java
+++ src/PlayerData.java
@@ -1,15 +1,27 @@
-
-
 public class PlayerData {
-    public int characterId;
-    public int pointX, pointY;
-    public int cash;
+    private int characterId;
+    private int position;
+    private int cash;
+    private boolean is_active;
     //건물 클래스
+
 
     public PlayerData(int characterId){
         this.characterId = characterId;
-        this.pointX = 0;
-        this.pointY = 0;
+        this.position = 0;
         this.cash = 0;
+        this.is_active = true;
+    }
+    public boolean getStatus(){
+        return this.is_active;
+    }
+    public int getCharacterIdI(){
+        return this.characterId;
+    }
+    public int getPosition(){
+        return this.position;
+    }
+    public  int getCash(){
+        return this.cash;
     }
 }
