114b0dabff83ec1a1f20c5c85810b53acb7eef01
James <jamesj0107@naver.com>
2018-12-03 00:40:03 +0900

    Add Game class to Main Panel

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main extends JPanel {
     private GameBoard gameBoard;
     private ScoreBoard scoreBoard;
     private StartPanel startPanel;
+    private Game game;
+    private StartController startController;
 
     public Main() {
         setPreferredSize(new Dimension(800, 750));
@@ -22,7 +24,9 @@ public class Main extends JPanel {
         startPanel.setVisible(true);
         add(startPanel);
 
-        StartController startController = new StartController(startPanel, gameBoard, scoreBoard);
+        game = new Game();
+
+        startController = new StartController(startPanel, gameBoard, scoreBoard, game);
 
     }
 }
