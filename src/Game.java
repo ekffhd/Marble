2ea05c85653c3da1837360badf6c6c6d04083d69
James <jamesj0107@naver.com>
2018-12-03 01:00:47 +0900

    Add function next() to swap to next player

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,9 @@
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-
 public class Game {
     private Dice dice1, dice2;
     private Player[] player;
     private Phase phase;
-    private int player_turn;
     private PhaseListener phaseListener;
+    private int player_turn;
 
     public Game(){
         phase = new Phase();
@@ -28,34 +25,13 @@ public class Game {
         phase.start();
     }
 
-    public int rollDice(){
-        return dice1.roll_dice() + dice2.roll_dice();
+    public void next(){
+        while (this.player[this.player_turn].getStatus()){
+            this.player_turn++;
+        }
     }
 
-    public class PhaseListener implements PropertyChangeListener{
-        @Override
-        public void propertyChange(PropertyChangeEvent event) {
-            if (event.getPropertyName().equals("START")){
-                System.out.println("start");
-            }
-            else if (event.getPropertyName().equals("BEFORE_START")){
-                System.out.println("before start");
-            }
-            else if (event.getPropertyName().equals("ROLL")){
-                System.out.println("roll");
-            }
-            else if (event.getPropertyName().equals("ACQUIRE")){
-                System.out.println("acquire");
-            }
-            else if (event.getPropertyName().equals("BILL")){
-                System.out.println("bill");
-            }
-            else if (event.getPropertyName().equals("END")){
-                System.out.println("end");
-            }
-            else if (event.getPropertyName().equals("RESTART")){
-                System.out.println("restart");
-            }
-        }
+    public int rollDice(){
+        return dice1.roll_dice() + dice2.roll_dice();
     }
 }
